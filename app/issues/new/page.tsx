51f9ee8d9e478e8a4709d55d55b1b4ac8e72c3b5
Abdidawud <abdidawud995@gmail.com>
2025-02-11 20:53:59 +0300

    Handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+import { Button, Callout, TextArea, TextField } from '@radix-ui/themes'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
@@ -13,17 +13,29 @@ interface IssueForm{
 const NewIssuesPage = () => {
   const {register,handleSubmit}= useForm<IssueForm>();
   const router=useRouter()
+  const [error,setError]=useState('')
   return (
-    <form className='max-w-xl space-y-3' 
+    <div className='max-w-xl'>
+      { error && <Callout.Root color='red' className='mb-5'>
+        <Callout.Text>{error}</Callout.Text>
+      </Callout.Root>
+      }
+    <form className='space-y-3' 
       onSubmit={handleSubmit(async(data)=> {
-        await axios.post('/api/issues',data)
-        router.push('/issues')}
+        try {
+          await axios.post('/api/issues',data)
+          router.push('/issues')
+          
+        } catch (error) {
+          setError('An unexpected error occured.')
+        }}
     )}>
       <TextField.Root placeholder='Title' {...register('title')}>
       </TextField.Root>
       <TextArea placeholder='Description' {...register('description')}/>
       <Button>Submit New Issue</Button>
     </form>
+    </div>
   )
 }
 
