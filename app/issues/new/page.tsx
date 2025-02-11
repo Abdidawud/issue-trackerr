78c36643d5c7de85670f159b82731de19afbbcf6
Abdidawud <abdidawud995@gmail.com>
2025-02-11 19:33:50 +0300

    Handle form submit

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,14 +1,29 @@
+'use client'
 import { Button, TextArea, TextField } from '@radix-ui/themes'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
+
+interface IssueForm{
+  title:string;
+  description:string;
+}
 const NewIssuesPage = () => {
+  const {register,handleSubmit}= useForm<IssueForm>();
+  const router=useRouter()
   return (
-    <div className='max-w-xl space-y-3'>
-      <TextField.Root placeholder='Title'>
+    <form className='max-w-xl space-y-3' 
+      onSubmit={handleSubmit(async(data)=> {
+        await axios.post('/api/issues',data)
+        router.push('/issues')}
+    )}>
+      <TextField.Root placeholder='Title' {...register('title')}>
       </TextField.Root>
-      <TextArea placeholder='Description'/>
+      <TextArea placeholder='Description' {...register('description')}/>
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   )
 }
 
