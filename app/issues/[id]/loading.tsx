903974412d6a1c2eddcdfbfc1f9144cbe1974c88
Abdidawud <abdidawud995@gmail.com>
2025-03-10 23:38:18 +0300

    Adding more skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import IssuesStatusBadge from "@/app/components/IssuesStatusBadge";
+import { Card, Flex, Heading } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssuesPage = () => {
   return (
-    <div>Loading.....</div>
-  )
-}
+    <div>
+      <Skeleton className="max-w-xl"></Skeleton>
+      <Flex my="3" className="space-x-3">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem  " />
+      </Flex>
+      <Card className="prose mt-5">
+        <Skeleton count={3} />
+      </Card>
+    </div>
+  );
+};
 
-export default LoadingIssuesPage
+export default LoadingIssuesPage;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,36 +1,33 @@
-import IssuesStatusBadge from '@/app/components/IssuesStatusBadge';
-import { prisma } from '@/prisma/client';
-import { Card, Flex, Heading, Text } from '@radix-ui/themes';
-import { notFound } from 'next/navigation';
-import React from 'react';
-import ReactmarkDown from 'react-markdown'
+import IssuesStatusBadge from "@/app/components/IssuesStatusBadge";
+import { prisma } from "@/prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import React from "react";
+import ReactmarkDown from "react-markdown";
 
 interface Props {
   params: { id: string };
 }
 
 const IssuesDetailPage = async ({ params }: Props) => {
-    
-        
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
 
   if (!issue) {
-    notFound()
+    notFound();
   }
 
   return (
     <div>
-     <Heading>{issue.title}</Heading>
-     <Flex my='3' className='space-x-3'>
-     <IssuesStatusBadge status={issue.status}/>
-     <Text>{issue.createdAt.toDateString()}</Text>
-     </Flex>
-     <Card className='prose mt-5'>
-     <ReactmarkDown>{issue.description}</ReactmarkDown>
-     </Card>
-
+      <Heading>{issue.title}</Heading>
+      <Flex my="3" className="space-x-3">
+        <IssuesStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose mt-5">
+        <ReactmarkDown>{issue.description}</ReactmarkDown>
+      </Card>
     </div>
   );
 };
